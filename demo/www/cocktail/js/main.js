c2f1bbc7ca30dff62a300b8fe857104e6558477b
nybras <nybras@codeine.it>
2009-03-05 12:00:07 -0300

    fixing demo javascript & config files

--- demo/www/cocktail/js/main.js
+++ demo/www/cocktail/js/main.js
@@ -1,32 +1,59 @@
-function updateMovieHeight ( value )
-{
-	value = Math.max ( value, window.innerHeight );
-	document.getElementById("content").style.height = value + "px";
-}
+function getWindowHeight() {
+  var windowHeight = 0;
+	
+  if (typeof(window.innerHeight) == 'number')
+    windowHeight = window.innerHeight;
+	
+  else {
+		
+    if (document.documentElement && document.documentElement.clientHeight)
+      windowHeight = document.documentElement.clientHeight;
+		
+    else {
+      if (document.body && document.body.clientHeight)
+        windowHeight = document.body.clientHeight; }; };
+				
+  return windowHeight;
+};
 
-function setScroll ( x, y )
-{
-	window.scroll(
-		(isNaN( x ) ? 0 : x ),
-		(isNaN( y ) ? 0 : y )
-	);
-}
+function getWindowWidth() {
+  var windowWidth = 0;
+	
+  if (typeof(window.innerWidth) == 'number')
+    windowWidth = window.innerWidth;
+	
+  else {
+		
+    if (document.documentElement && document.documentElement.clientWidth)
+      windowWidth = document.documentElement.clientWidth;
+		
+    else {
+      if (document.body && document.body.clientWidth)
+        windowWidth = document.body.clientWidth; }; };
+				
+  return windowWidth;
+};
 
 
-function openGiftCard (  )
+function forcesize ()
 {
-	var centerWidth = (window.screen.width - 749) / 2;
-    var centerHeight = (window.screen.height - 381) / 2;
-	window.open("http://www.gemx.ca/ub/ubc.html","giftcard","height=381,width=749,left="+centerWidth+",top="+centerHeight);
-	//window.open("http://www.gemx.ca/ub/ubc.html","giftcard","height=381,width=749");
-}
-
-function window_height ()
-{
-	return window.innerHeight;
+	if ( getWindowWidth() < 820)
+		document.getElementById("content").style.width = "800px";
+	else
+		document.getElementById("content").style.width = "100%";
+	
+	if ( getWindowHeight() < 720)
+		document.getElementById("content").style.height = "720px";
+	else
+		document.getElementById("content").style.height = "100%";
 }
 
 window.onload = function()
 {
-	updateMovieHeight(window.innerHeight - 5);
+	forcesize();
+}
+
+window.onresize = function()
+{
+	forcesize();
 }
