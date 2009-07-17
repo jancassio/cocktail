531a5563e75114e302f07fe7e0bf0c0570f0e26a
hems <henrique@codeine.it>
2009-07-17 16:47:36 -0300

    adding "create_area" script.

--- scripts/create_embedder.rb
+++ scripts/create_embedder.rb
@@ -2,15 +2,14 @@
 require 'pathname'
 
 BASE_DIR = Pathname.new( File.dirname(__FILE__) + "/.." ).cleanpath
-SRC_PATH = "#{ARGV[0]}/#{ARGV[1]}"
-PRJ_NAME = ARGV[1];
-EMB_PATH = BASE_DIR, SRC_PATH + "/boot/Embedder.as"
+SRC_PATH = BASE_DIR + "#{ARGV[0]}/#{ARGV[1]}"
+PRJ_NAME = ARGV[1]
+EMB_PATH = File.join( SRC_PATH, "/boot/Embedder.as" )
 
 #list all views/controller/models in the application
 #convert filepath to classpaths
 
-lines = Dir[ File.join( BASE_DIR, SRC_PATH + "/**/*.as" ) ]\
-.grep( %r[.*/(views|controllers|models)] )\
+lines = Dir[ File.join( SRC_PATH, "/**/*.as" ) ].grep( %r[.*/(views|controllers|models)] )\
 .map{ |s| 
 	s[ s.index( PRJ_NAME), s.length ].gsub( "/", "." ).gsub( ".as", ";" ) 
 }
@@ -18,12 +17,12 @@ lines = Dir[ File.join( BASE_DIR, SRC_PATH + "/**/*.as" ) ]\
 #read template
 #replace stubs
 out = IO.read( File.join( BASE_DIR, "scripts/templates/Embedder.as" ) )
-out = out.sub( "PROJECT_NAME", PRJ_NAME )
-out = out.sub( /^(\s*)YOUR_CODE_HERE/ ) {
+out = out.gsub( "PROJECT_NAME", PRJ_NAME )
+out = out.gsub( /^(\s*)YOUR_CODE_HERE/ ) {
   lines.map { |s|
     [$1, s].join("")
   }.join("\n")
 }
 
 #save file
-File.open( File.join( EMB_PATH ), 'w' ) { |f| f.write out }
+File.open( EMB_PATH, 'w' ) { |f| f.write out }
