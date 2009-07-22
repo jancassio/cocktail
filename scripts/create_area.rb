4b36d35a1005e819893024703778257fbb54cfeb
hems <henrique@codeine.it>
2009-07-22 12:57:01 -0300

    created shake script.

--- scripts/create_area.rb
+++ scripts/create_area.rb
@@ -2,6 +2,7 @@
 require 'pathname'
 require 'rubygems'
 require 'active_support/inflector'
+require File.dirname($0) + '/shake'
 
 BASE_DIR = Pathname.new( File.dirname(__FILE__) + "/.." ).cleanpath
 SRC_PATH = File.join( BASE_DIR, "#{ARGV[0]}/#{ARGV[1]}" )
@@ -10,29 +11,30 @@ TMPL_DIR = File.join( BASE_DIR, "scripts/templates/area/" )
 
 #use shell to get area name from user
 puts "Please inform the area name: "
-
-#TODO: CHECK WHY THE FUCKING GETS ISNT WORKING HERE
-#I THINK THAT IS A PROBLEM WITH FILE, ITS TRYING TO JOIN THE PATH BEFORE AREA_NAME GETS RETURNS
 AREA_NAME = STDIN.gets.chomp
 
 #read / supplant Controller template
 CONTROLLER_NAME = AREA_NAME
 CONTROLLER_PATH = File.join( SRC_PATH, "/controllers/" + CONTROLLER_NAME + "Controller.as" )
 
-out = IO.read( File.join( TMPL_DIR, "/Controller.as" ) )
-out = out.gsub( "PROJECT_NAME", PRJ_NAME );
-out = out.gsub( "CONTROLLER_NAME", CONTROLLER_NAME )
-
-#save file
-File.open( CONTROLLER_PATH, 'w' ) { |f| f.write out }
+Shake.supplant(
+  File.join( TMPL_DIR, "/Controller.as" ),
+  CONTROLLER_PATH,
+  {
+    "PROJECT_NAME" => PRJ_NAME,
+    "CONTROLLER_NAME" => CONTROLLER_NAME 
+  }
+)
 
 #read / supplant Model template
 MODEL_NAME = Inflector.singularize( AREA_NAME )
-MODEL_PATH = File.join( SRC_PATH, "/models/" + CONTROLLER_NAME + "Model.as" )
+MODEL_PATH = File.join( SRC_PATH, "/models/" + MODEL_NAME + "Model.as" )
 
-out = IO.read( File.join( TMPL_DIR, "Model.as" ) )
-out = out.gsub( "PROJECT_NAME", PRJ_NAME );
-out = out.gsub( "MODEL_NAME", MODEL_NAME )
-
-#save file
-File.open( File.join( SRC_PATH, "/models/" + MODEL_NAME + "Model.as" ), 'w' ) { |f| f.write out }
+Shake.supplant(
+  File.join( TMPL_DIR, "Model.as" ), 
+  MODEL_PATH, 
+  {
+    "PROJECT_NAME" => PRJ_NAME,
+    "MODEL_NAME" => MODEL_NAME
+  }
+)
--- scripts/create_embedder.rb
+++ scripts/create_embedder.rb
@@ -1,28 +1,28 @@
 #!/usr/bin/env ruby
 require 'pathname'
+require File.dirname($0) + '/shake'
 
 BASE_DIR = Pathname.new( File.dirname(__FILE__) + "/.." ).cleanpath
 SRC_PATH = BASE_DIR + "#{ARGV[0]}/#{ARGV[1]}"
 PRJ_NAME = ARGV[1]
-EMB_PATH = File.join( SRC_PATH, "/boot/Embedder.as" )
 
-#list all views/controller/models in the application
-#convert filepath to classpaths
+#find and list app views/controller/models
+#then convert filepath to classpaths
 
 lines = Dir[ File.join( SRC_PATH, "/**/*.as" ) ].grep( %r[.*/(views|controllers|models)] )\
 .map{ |s| 
-	s[ s.index( PRJ_NAME), s.length ].gsub( "/", "." ).gsub( ".as", ";" ) 
+	s[ s.index( PRJ_NAME ), s.length ].gsub( "/", "." ).gsub( ".as", ";" ) 
 }
 
-#read template
-#replace stubs
-out = IO.read( File.join( BASE_DIR, "scripts/templates/Embedder.as" ) )
-out = out.gsub( "PROJECT_NAME", PRJ_NAME )
-out = out.gsub( /^(\s*)YOUR_CODE_HERE/ ) {
-  lines.map { |s|
-    [$1, s].join("")
-  }.join("\n")
-}
+#add \t to feat the template formmatting
+lines = lines.map { |s| [ "\t\t\t", s].join("") }.join("\n")
 
-#save file
-File.open( EMB_PATH, 'w' ) { |f| f.write out }
+#write Embedder Class
+Shake::supplant( 
+  File.join( BASE_DIR, "scripts/templates/Embedder.as" ), 
+  File.join( SRC_PATH, "/boot/Embedder.as" ), 
+  { 
+    "PROJECT_NAME" => PRJ_NAME,
+    /^(\s*)YOUR_CODE_HERE/ => lines
+  }
+)
